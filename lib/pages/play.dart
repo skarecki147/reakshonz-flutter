869bd3f4a501e758dac8945f383f5649a842176b
Seweryn Karecki <103260377+sewkarecki147@users.noreply.github.com>
2022-05-13 21:46:32 +0100

    flutter changes to play view

--- lib/pages/play.dart
+++ lib/pages/play.dart
@@ -1,7 +1,12 @@
+
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class Play extends StatelessWidget {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) => Scaffold(
         body: WebView(
